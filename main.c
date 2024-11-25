06fb78cf8177b64d98d2a3e48994670b7cdbf26f
sebgrgs <sebastien.georges11@gmail.com>
2024-11-25 16:13:14 +0100

    cleanup: remove unused variables and streamline printf calls in main function

--- main.c
+++ main.c
@@ -9,19 +9,17 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
-    unsigned int ui;
-    void *addr;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
+ 
+
+   _printf("Let's try to printf a simple sentence.\n");
+    printf("Let's try to printf a simple sentence.\n");
     _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
+    _printf("Percent:[%%]\n");
+   printf("Percent:[%%]\n");
 
     return (0);
 }
