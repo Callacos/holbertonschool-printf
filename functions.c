2e2885c68c084f09bc675365c54d59d256f29fde
sebgrgs <sebastien.georges11@gmail.com>
2024-11-25 14:20:51 +0100

    functions choosed by format

--- functions.c
+++ functions.c
@@ -1,22 +1,26 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * print_char - print a charactere c
-*@ap: argument
+* @ap: argument
 *
 * Return: 0
 */
+
 void print_char(va_list ap)
 {
-	_putchar(va_arg(ap, int))
+	_putchar(va_arg(ap, int));
 	return (0);
 }
+
 /**
 * print_string - print a string with a charactere s
-*@ap: argument
+* @ap: argument
 *
 * Return: 0
 */
+
 void print_string(va_list ap)
 {
 	_putchar(va_arg(ap, char *));
@@ -27,8 +31,9 @@ void print_string(va_list ap)
 * print_porcent - print a pourcent with a charactere %
 * @ap: argument
 *
-*Return: 0
+* Return: 0
 */
+
 void print_porcent(va_list ap)
 {
 	_putchar(va_arg(ap, int));
