a09d02af007da762ae2a0144fda7de2dc3bcbea1
Callacos <callacos74@gmail.com>
2024-11-25 13:55:45 +0100

    first commit to functions.c

---
+++ functions.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_char - print a charactere c
+*@ap: argument
+*
+* Return: 0
+*/
+void print_char(va_list ap)
+{
+	_putchar(va_arg(ap, int))
+	return (0);
+}
+/**
+* print_string - print a string with a charactere s
+*@ap: argument
+*
+* Return: 0
+*/
+void print_string(va_list ap)
+{
+	_putchar(va_arg(ap, char *));
+	return (0);
+}
+
+/**
+* print_porcent - print a pourcent with a charactere %
+* @ap: argument
+*
+*Return: 0
+*/
+void print_porcent(va_list ap)
+{
+	_putchar(va_arg(ap, int));
+	return (0);
+}
